5b4f3d5f5941e2fc9e2d8daaa8c9c128d2ee1939
Sai Kiran Budumuru <166619746+saikiran-011@users.noreply.github.com>
2025-08-23 21:45:21 +0530

    Add files via upload

---
+++ HTML-CSS/script.js
@@ -0,0 +1,7 @@
+// FAQ Toggle
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+    answer.style.display = (answer.style.display === "block") ? "none" : "block";
+  });
+});
